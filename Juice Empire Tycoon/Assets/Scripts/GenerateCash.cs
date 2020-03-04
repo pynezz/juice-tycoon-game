b5acd7bed567d7b74dab23e373f7a59287b15c16
Kevin <kevin.ka_96@hotmail.com>
2020-03-04 21:27:33 +0100

    Setting things up
    Set up a basic HUD to test scripting. Button now generates "one cash" and shows it to  the user at the top of the screen.
    
    Some graphics added.
    Folders added.
    
    Setting things up...to be continued.

---
+++ Juice Empire Tycoon/Assets/Scripts/GenerateCash.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateCash : MonoBehaviour
+{
+    public GlobalCash globalCash;
+    private float plusOneCash = 1f;
+
+    public void GetCash()
+    {
+        globalCash.cashCount += plusOneCash;
+    }
+}
---
+++ Juice Empire Tycoon/Assets/Scripts/GlobalCash.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalCash : MonoBehaviour
+{
+    public float cashCount;
+    private float internalCash;
+
+    public GameObject cashText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        internalCash = cashCount;
+        cashText.GetComponent<Text>().text = "" + internalCash;
+    }
+}
