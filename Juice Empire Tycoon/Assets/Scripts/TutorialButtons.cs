d8419fbfeb5f728aedb26c3b1ce3b943f8389037
Kevin <kevin.ka_96@hotmail.com>
2020-03-04 22:00:46 +0100

    Welcome screen
    Added a welcome screen

---
+++ Juice Empire Tycoon/Assets/Scripts/TutorialButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialButtons : MonoBehaviour
+{
+    public GameObject welcomeScreen;
+
+    public void Start()
+    {
+        //welcomeScreen.SetActive(true);
+    }
+
+    public void ExitButton()
+    {
+        welcomeScreen.SetActive(false);
+    }
+
+    public void StartPlaying()
+    {
+        welcomeScreen.SetActive(false);
+    }
+}
