2be9ecab79ecb2f0176214843431daf15f6c757d
Kevin <kevin.ka_96@hotmail.com>
2020-03-05 21:47:51 +0100

    script update

--- Juice Empire Tycoon/Assets/Scripts/ItemAssets.cs
+++ Juice Empire Tycoon/Assets/Scripts/ItemAssets.cs
@@ -18,20 +18,21 @@ public class ItemAssets : MonoBehaviour
     public Sprite radiationJuiceSprite;
     public Sprite premiumKingJuiceSprite;
     public Sprite emptyBottleSprite;
- /*    
     public Sprite nicotine100mgSprite;
-    public Sprite PG100mlSprite;
-    public Sprite PG500mlSprite;
-    public Sprite PG1000mlSprite;
-    public Sprite PG5000mlSprite;
-    public Sprite PG10LSprite;
-    public Sprite PG50LSprite;
-    public Sprite VG100mlSprite;
-    public Sprite VG500mlSprite;
-    public Sprite VG1000mlSprite;
-    public Sprite VG5000mlSprite;
-    public Sprite VG10LSprite;
-    public Sprite VG50LSprite;
-    public Sprite scaleSprite;  
-*/
+    /*    
+
+       public Sprite PG100mlSprite;
+       public Sprite PG500mlSprite;
+       public Sprite PG1000mlSprite;
+       public Sprite PG5000mlSprite;
+       public Sprite PG10LSprite;
+       public Sprite PG50LSprite;
+       public Sprite VG100mlSprite;
+       public Sprite VG500mlSprite;
+       public Sprite VG1000mlSprite;
+       public Sprite VG5000mlSprite;
+       public Sprite VG10LSprite;
+       public Sprite VG50LSprite;
+       public Sprite scaleSprite;  
+   */
 }
