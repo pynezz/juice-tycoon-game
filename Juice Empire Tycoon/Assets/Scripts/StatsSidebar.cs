028ee593dd19f20669563fdb286f009885c1912c
Kevin <kevin.ka_96@hotmail.com>
2020-03-05 20:58:29 +0100

    sidebarscript
    added

---
+++ Juice Empire Tycoon/Assets/Scripts/StatsSidebar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsSidebar : MonoBehaviour
+{
+    public GameObject icyJuiceCount, thunderJuiceCount, cloverJuiceCount, radiateJuiceCount, kingJuiceCount, emptyBottleCount, nicotineCount, VGJuiceCount, PGJuiceCount; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
