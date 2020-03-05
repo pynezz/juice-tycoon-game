52af914527564e34b922f27974fe0fe2a0c04eca
Kevin <kevin.ka_96@hotmail.com>
2020-03-05 19:57:18 +0100

    More inventory
    some buttoncode and added inventory layout

---
+++ Juice Empire Tycoon/Assets/Scripts/Buttons.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public GameObject exitInventoryBtn;
+    public GameObject totalInventoryUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InventoryKeyCode();
+    }
+
+    public void CloseInventoryWindow()
+    {
+        totalInventoryUI.SetActive(false);
+    }
+
+    public void OpenInventoryButtonPress()
+    {
+        totalInventoryUI.SetActive(!totalInventoryUI.activeSelf);
+    }
+
+    public void InventoryKeyCode()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            totalInventoryUI.SetActive(!totalInventoryUI.activeSelf);
+        }
+       
+    }
+}
--- Juice Empire Tycoon/Assets/Scripts/Game_Manager.cs
+++ Juice Empire Tycoon/Assets/Scripts/Game_Manager.cs
@@ -7,9 +7,21 @@ public class Game_Manager : MonoBehaviour
 {
     private Inventory inventory;
 
+    public GameObject fullInventoryUI;
+
     [SerializeField] private UI_Inventory uiInventory;
 
-
+    public void Start()
+    {
+        if(Input.GetKeyDown(KeyCode.I) && fullInventoryUI.activeSelf)
+        {
+            fullInventoryUI.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.I) && !fullInventoryUI.activeSelf)
+        {
+            fullInventoryUI.SetActive(true);
+        }
+    }
 
     private void Awake()
     {
